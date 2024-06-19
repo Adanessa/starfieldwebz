d5ca0454063b966936d3e8ab84ac6b9c8903c6d6
Adanessa <148044046+Adanessa@users.noreply.github.com>
2024-06-19 21:41:41 +0200

    Todd

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,10 +1,19 @@
 window.onscroll = function() { 
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
+    myFunction();
+};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
+function myFunction() {
     if (window.scrollY >= sticky) {
         navbar.classList.add("sticky");
+        document.body.style.paddingTop = navbar.offsetHeight + 'px';
     } else {
+        navbar.classList.remove("sticky");
+        document.body.style.paddingTop = '0'
+    }
+
+    if (window.scrollYOffset === 0) {
         navbar.classList.remove("sticky");
     }
 };
