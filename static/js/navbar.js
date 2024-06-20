1647233402806f826546bbfab1f1e4e2c29ee845
Adanessa <tomspanx@gmail.com>
2024-06-20 09:02:13 +0200

    Update navbar.js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,7 +1,7 @@
 window.onscroll = function() { 
     myFunction();
 };
-var navbar = document.getElementById("navbar");
+const navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
