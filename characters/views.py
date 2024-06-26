ef82764795cf81602b397ab61b581b098260e1da
Adanessa <tomspanx@gmail.com>
2024-06-26 11:20:10 +0200

    character updates

--- characters/views.py
+++ characters/views.py
@@ -11,8 +11,20 @@ def andreja_detail(request):
     skills = andreja.skills.all()
     return render(request, 'andreja.html', {'character': andreja, 'skills': skills})
 
+def amundsen_barret(request):
+    amundsen_barret = get_object_or_404(Character, id=2)
+    skills = amundsen_barret.skills.all()
+    return render(request, 'amundsen_barret.html', {'character': amundsen_barret, 'skills': skills})
 
+def same_coe(request):
+    same_coe = get_object_or_404(Character, id=3)
+    skills = same_coe.skills.all()
+    return render(request, 'sam_coe.html', {'character': same_coe, 'skills': skills})
 
+def heller(request):
+    heller = get_object_or_404(Character, id=5)
+    skills = heller.skills.all()
+    return render(request, 'heller.html', {'character': heller, 'skills': skills})
 # def character_info(request):
 #     character_details = Character.objects.all()
 #     character_skills = Skill.objects.all()
