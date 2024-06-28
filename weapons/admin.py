d80d6d38b8721ad1ddb24088f7a105afe7157833
Adanessa <tomspanx@gmail.com>
2024-06-28 11:20:56 +0200

    added weapon images

--- weapons/admin.py
+++ weapons/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import AidItems, Weapons, AllWeaponModPrefixes, AllWeaponPerks
 
 admin.site.register(AidItems)
-admin.site.register(Weapons)
 admin.site.register(AllWeaponModPrefixes)
 admin.site.register(AllWeaponPerks)
+
+class WeaponsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rarity', 'type', 'dmg', 'ammo', 'mag', 'fire_rate', 'range', 'accuracy', 'mass', 'mod_capacity', 'image_filename')
+
+admin.site.register(Weapons, WeaponsAdmin)
